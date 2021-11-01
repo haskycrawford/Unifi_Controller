d7bb47fc54cc0f68dbe43e15c1f674699d38224a
KFX <KLEITON.FREITAS@LIVE.COM>
2021-11-01 16:56:37 -0300

    Up
    up

--- install_unifi_controller.sh
+++ install_unifi_controller.sh
@@ -1,5 +1,5 @@
 apt-get update 
 apt-get install ca-certificates wget -y
-wget https://get.glennr.nl/unifi/install/unifi-5.11.39.sh
+wget https://get.glennr.nl/unifi/install/unifi-6.4.54.sh
 chmod +x unifi-5.11.39.sh
 ./unifi-5.11.39.sh
